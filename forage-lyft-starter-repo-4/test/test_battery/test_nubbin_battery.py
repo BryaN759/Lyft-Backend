d156bdc2a9452a24f473b68d44c08c0c99005ae9
BryaN759 <khaledmushahed@gmail.com>
2023-10-28 19:52:01 +0600

    TDD

---
+++ forage-lyft-starter-repo-4/test/test_battery/test_nubbin_battery.py
@@ -0,0 +1,18 @@
+import unittest
+from datetime import date
+
+from battery.nubbin_battery import NubbinBattery
+
+
+class TestNubbinBattery(unittest.TestCase):
+    def test_needs_service_true(self):
+        current_date = date.fromisoformat("2023-10-28")
+        last_service_date = date.fromisoformat("2021-04-17")
+        battery = NubbinBattery(current_date, last_service_date)
+        self.assertTrue(battery.needs_service())
+
+    def test_needs_service_false(self):
+        current_date = date.fromisoformat("2023-10-28")
+        last_service_date = date.fromisoformat("2019-05-22")
+        battery = NubbinBattery(current_date, last_service_date)
+        self.assertFalse(battery.needs_service())
